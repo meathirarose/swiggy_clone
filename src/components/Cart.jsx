aa67e3b695e5cb517d14a45b70ab969f790f54e9
meathirarose <athirarosejohn@gmail.com>
2024-08-28 16:32:52 +0530

    cart updates

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import ItemList from './ItemList';
+import { clearCart } from '../utils/cartSlice';
 
 const Cart = () => {
 
   const cartItems = useSelector((store) => store.cart.items);
 
+  const dispatch = useDispatch();
+
+  const handleClearCart = ()=>{
+    dispatch(clearCart())
+  }
+
   return (
     <div className='text-center m-4 p-4'>
         <h1 className='text-2xl font-bold'>Cart</h1>
         <div className='w-6/12 m-auto'>
+          <button className='px-5 py-1 my-3 bg-black text-white rounded-lg'
+            onClick={handleClearCart}
+          >
+            Clear Cart 
+          </button>
+          {cartItems.length === 0 && 
+            <h1>Your cart is empty! Please add items to the cart!</h1>
+          }
           <ItemList items={cartItems} />
         </div>
     </div>
