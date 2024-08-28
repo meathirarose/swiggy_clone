a20414ee80130ad15728660270035555cbbf2c42
meathirarose <athirarosejohn@gmail.com>
2024-08-28 15:15:03 +0530

    added items to the cart and showed in the cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import ItemList from './ItemList';
 
 const Cart = () => {
+
+  const cartItems = useSelector((store) => store.cart.items);
+
   return (
-    <div>
-        <h1>Cart</h1>
-        <h2>This is an cart page.</h2>
+    <div className='text-center m-4 p-4'>
+        <h1 className='text-2xl font-bold'>Cart</h1>
+        <div className='w-6/12 m-auto'>
+          <ItemList items={cartItems} />
+        </div>
     </div>
   )
 }
